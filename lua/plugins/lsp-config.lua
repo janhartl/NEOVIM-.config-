eaebb3089726346363fb36b4a7ed014a00406296
janhartl <janhartl@icloud.com>
2025-10-14 13:51:42 +0200

    Fixing issue of require(' lspconfig ') becoming deprecated in favor of vim.lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,54 +1,50 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        opts = {
-            auto_install = true,
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                "folke/lazydev.nvim",
-                ft = "lua",
-                opts = {
-                    library = {
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                    },
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
                 },
             },
-            "saghen/blink.cmp",
         },
-        opts = {
-            servers = {
-                lua_ls = {},
-                texlab = {},
-                jedi_language_server = {},
-                ast_grep = {},
-                clangd = {},
-                solargraph = {},
-            },
-        },
-        config = function(_, opts)
-            local lspconfig = require("lspconfig")
-            for server, config in pairs(opts.servers) do
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-                vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-                vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-                -- passing config.capabilities to blink.cmp merges with the capabilities in your
-                -- `opts[server].capabilities, if you've defined it
-                config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
-            end
-        end,
+        "saghen/blink.cmp",
     },
+    opts = {
+        servers = {
+            lua_ls = {},
+            texlab = {},
+            jedi_language_server = {},
+            ast_grep = {},
+            clangd = {},
+            solargraph = {},
+        },
+    },
+    config = function(_, opts)
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+        local on_attach = function(_, bufnr)
+            local keymap = vim.keymap.set
+            local opts = { buffer = bufnr, silent = true }
+            keymap("n", "K", vim.lsp.buf.hover, opts)
+            keymap("n", "<leader>gd", vim.lsp.buf.definition, opts)
+            keymap("n", "<leader>gr", vim.lsp.buf.references, opts)
+            keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        end
+
+        -- Loop through servers
+        for server, config in pairs(opts.servers) do
+            vim.lsp.config(
+                server,
+                vim.tbl_deep_extend("force", config, {
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                })
+            )
+
+            vim.lsp.enable(server)
+        end
+    end,
 }
